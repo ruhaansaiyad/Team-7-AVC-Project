3cebf0050264799227e02783068734a4813fda34
MingShaneTong <63452934+MingShaneTong@users.noreply.github.com>
2020-06-14 20:46:07 +1200

    Add files via upload

--- AVC_robot/ImageProcessing.hpp
+++ AVC_robot/ImageProcessing.hpp
@@ -1,11 +1,47 @@
 class ImageProcessing{
 	public:
+		int getWallCurve(ImagePPM image);
 		int* getCenterRow(ImagePPM image);
+		int getExit(ImagePPM image);
+		int hasWall(ImagePPM image);
+		int hasObstacle(ImagePPM image);
 	private:
 		int getColor(ImagePPM image, int row, int col);
 		int isWhite(ImagePPM image, int row, int col);
-		//int isRed(ImagePPM image, int row, int col);
+		int isRed(ImagePPM image, int row, int col);
 };
+
+/**
+ * returns the edge of the frame
+ * -1 - none
+ * 0 - left
+ * 1 - top
+ * 2 - right
+ */
+int ImageProcessing::getExit(ImagePPM image){	
+	// look at left edge, bottom to top
+	for(int i = image.height-1; i >= 0; i--){
+		if(getColor(image, i, 0) == 1){
+			return 0; 
+		}
+	}
+	
+	// looks at top edge, left to right
+	for(int i = 0; i < image.width; i++){
+		if(getColor(image, 0, i) == 1){
+			return 1;
+		}
+	}
+	
+	// look at right edge, top to bottom
+	for(int i = 0; i < image.height; i++){
+		if(getColor(image, i, image.height-1) == 1){
+			return 2;
+		}
+	}
+	
+	return -1;
+}
 
 /**
  * returns a summary of the center row
@@ -22,13 +58,64 @@ int* ImageProcessing::getCenterRow(ImagePPM image){
 }
 
 /**
+ * returns 01 based on if there is a wall on the robots left
+ */
+int ImageProcessing::hasWall(ImagePPM image){
+	// looks at left edge and bottom left pixels for red pixels
+	// look at left edge
+	for(int i = 0; i < image.height; i++){
+		if(getColor(image, i, 0) == 2){
+			return 1; 
+		}
+	}
+	// looks at bottom left
+	for(int i = 0; i < image.width/2; i++){
+		if(getColor(image, image.height-1, i) == 2){
+			return 1; 
+		}
+	}
+	
+	return 0;
+}
+
+/**
+ * returns 01 based on if there is a wall in front
+ */
+int ImageProcessing::hasObstacle(ImagePPM image){
+	// bottom column
+	for(int i = image.height*3/4; i < image.height; i++){
+		if(getColor(image, i, image.width/2) == 2){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+/**
+ * returns the angle at which the robot should turn 
+ * to touch the next left wall after the left wall ends
+ */
+int ImageProcessing::getWallCurve(ImagePPM image){
+	// checks if there is a horizontal wall attached to the left wall
+	for(int i = 0; i < image.height; i++){
+		if(getColor(image, i, 0) == 2){
+			return 90;
+		}
+	}
+	return 180;
+}
+
+/**
  * returns a number for colour of the pixel
  * 0-not any of the following colours
  * 1-white
+ * 2-red
  */
 int ImageProcessing::getColor(ImagePPM image, int row, int col){
 	if(isWhite(image, row, col)){
 		return 1;
+	} else if(isRed(image, row, col)){
+		return 2;
 	} else {
 		return 0;
 	}
@@ -41,4 +128,15 @@ int ImageProcessing::isWhite(ImagePPM image, int row, int col){
 	// check white by looking at the luminocity
 	int lumin = get_pixel(image, row, col, 3);
 	return (int)(lumin > 240);
+}
+
+/**
+ * returns 0 or 1 based on if the pixel is red
+ */
+int ImageProcessing::isRed(ImagePPM image, int row, int col){
+	// check white by looking at the luminocity
+	int r = get_pixel(image, row, col, 0);
+	int g = get_pixel(image, row, col, 1);
+	int b = get_pixel(image, row, col, 2);
+	return (r/2 > b) && (r/2 > g) && (r > 10);
 }
